286941be24bfa8aa73fe88f0196d0c6e5d87f490
lukesuffert <lukesuffert@byui.edu>
2025-11-21 18:39:38 -0700

    sorting

--- ponder/sorting/sorting.js
+++ ponder/sorting/sorting.js
@@ -67,3 +67,32 @@ const hikes = [
   }
 ];
  const simpleList = ["oranges", "grapes", "lemons", "apples", "Bananas", "watermelons", "coconuts", "broccoli", "mango"];
+
+     function compareFn(a,b) {
+      if (a > b) {
+        return -1;
+      } else if (a < b) {
+        return 1;
+      }
+     // a must be equal to b
+     return 0;
+    }
+
+  function searchList(list, q) {
+    function searchCallback(item) {
+      return (
+        item.name.toLowerCase().includes(q.toLowerCase()) ||
+        item.description.toLowerCase().includes(q.toLowerCase()) ||
+        item.tags.find((tag) => tag.toLowerCase().includes(q.toLowerCase()))
+      );
+    }
+    const filtered = list.filter(searchCallback);
+
+    const sorted = filtered.sort((a, b) => a.distance > b.distance);
+    return sorted;
+  }
+  console.log(searchList(hikes, "yellowstone"));
+  console.log(searchList(hikes, "moderate"));
+  console.log(searchList(hikes, "al"));
+
+
